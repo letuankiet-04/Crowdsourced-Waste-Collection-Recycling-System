9fc112a0c283dee36f5e02f8aa90f6ef2457b3e1
Lê Tuấn Kiệt <lkiet2404.2005@gmail.com>
2026-01-21 00:43:45 +0700

    Config application (#1)

---
+++ src/main/java/com/team2/Crowdsourced_Waste_Collection_Recycling_System/CrowdsourcedWasteCollectionRecyclingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.team2.Crowdsourced_Waste_Collection_Recycling_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdsourcedWasteCollectionRecyclingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdsourcedWasteCollectionRecyclingSystemApplication.class, args);
+	}
+
+}
