5e320f7d6fd72807dad1feccc4636cbf7b57ad84
Lê Tuấn Kiệt <lkiet2404.2005@gmail.com>
2026-03-13 18:42:03 +0700

    fix

--- src/main/java/com/team2/Crowdsourced_Waste_Collection_Recycling_System/config/SecurityConfig.java
+++ src/main/java/com/team2/Crowdsourced_Waste_Collection_Recycling_System/config/SecurityConfig.java
@@ -33,6 +33,7 @@ import java.util.List;
 public class SecurityConfig {
     private final String[] PUBLIC_ENDPOINTS = {
         "/", "/api/auth/login", "/api/auth/register", "/api/auth/introspect", "/api/auth/logout",
+        "/actuator/health", "/actuator/info",
         "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs.yaml", "/swagger-resources/**", "/webjars/**"
     };
 
